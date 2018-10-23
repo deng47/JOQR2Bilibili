9e0f71f01c0cc878a2c67156e5188e0feff30677
Siqi Deng <SiqiDeng@lbl.gov>
2018-10-23 12:39:27 -0700

    debug

--- vtt2json.sh
+++ vtt2json.sh
@@ -7,7 +7,9 @@ echo '{"font_size":0.4,"font_color":"#FFFFFF","background_alpha":0.5,"background
     line=$(echo "$line" | sed 's/<[^>]*>//g' | sed 's/align.*//g')
     time=$(echo "$line" | grep ' --> ' | sed 's/ --> /,/g')
 
-    [ "$time" ] && [ "$output" ] && from=$(echo "$time" | cut -d',' -f1 |  awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') && to=$(echo "$time" | cut -d',' -f2 |  awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') && output="{\"from\":$from,\"to\":$to,\"location\":2,\"content\":\"$text\"}" && echo "$output" && text=''
+    [ "$time" ] && [ ! "$pretime" ] && pretime="$time" && continue
+
+    [ "$time" ] && [ "$output" ] && from=$(echo "$pretime" | cut -d',' -f1 |  awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') && to=$(echo "$pretime" | cut -d',' -f2 |  awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') && output="{\"from\":$from,\"to\":$to,\"location\":2,\"content\":\"$text\"}" && echo "$output" && pretime='' && text=''
 
     [ "$time" ] && output="Dialogue: 0,$time,Default,,0,0,0,,"
 
